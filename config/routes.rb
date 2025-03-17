6437d2c9d5e87d99ad323b5c9fda5855750b2588
AKI-86 <201310899+AKI-86@users.noreply.github.com>
2025-03-17 00:48:26 +0000

    [Finish] ファイルの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  root to: "homes#top"
+  devise_for :users
+
   get 'books/new'
   get 'books/index'
   get 'books/show'
   get 'books/edit'
-  get 'homes/top'
-  devise_for :users
-root to: "homes#top"
+
+  resources :books, only: [:new, :index, :show]
+
 end
