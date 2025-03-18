10714ddc2cda80838895a133749e3bab93c38fcc
AKI-86 <201310899+AKI-86@users.noreply.github.com>
2025-03-18 02:20:12 +0000

    [Finish] 名前でログインできるように設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-  root to: "homes#top"
   devise_for :users
+  root to: "homes#top"
+  get 'homes/about', to: 'homes#about', as: 'about'
 
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-
-  resources :books, only: [:new, :index, :show]
-
+  resources :books, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit, :index, :update]
 end
