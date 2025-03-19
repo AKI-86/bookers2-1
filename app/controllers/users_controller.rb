4b37d58771f9cefc5a2e756386a75c2bb86dc71b
AKI-86 <201310899+AKI-86@users.noreply.github.com>
2025-03-19 12:38:10 +0000

    [Finish] フラッシュメッセージの実装(itibu)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,8 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to @user, notice: 'User was successfully updated.'
+      flash[:notice] = "You have updated user successfully."
+      redirect_to @user
     else
       render :edit
     end
