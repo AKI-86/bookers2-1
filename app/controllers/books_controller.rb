061e88d7ad7d1c1f6cd740ae3e98c360a4f39557
AKI-86 <201310899+AKI-86@users.noreply.github.com>
2025-03-20 02:41:30 +0000

    [Finish]RSpecのテスト1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -24,21 +24,23 @@ class BooksController < ApplicationController
     @book.user_id = current_user.id
     if @book.save
       flash[:notice] = "You have created book successfully."
-      redirect_to book_path(@book.id)
+      redirect_to book_path(@book)
     else
+      @user = current_user
+      @books = Book.all
       render :index
     end
   end
 
   def update
     @book = Book.find(params[:id])
-  if @book.update(book_params)
-    flash[:notice] = "You have updated book successfully.."
-    redirect_to @book
-  else
-    render 'edit'
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully.."
+      redirect_to @book
+    else
+      render 'edit'
+    end
   end
-end
 
   def destroy
     book = Book.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get 'homes/about', to: 'homes#about', as: 'about'
 
+  authenticated :user do
+    root to: 'users#show', as: :authenticated_root
+  end
+
   resources :books, only: [:new, :edit, :create, :index, :show, :destroy, :update]
   resources :users, only: [:show, :edit, :index, :update]
 end
